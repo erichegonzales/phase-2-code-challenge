287c108fdc87f9667a802b71d78eaea46c99e4b3
Eriche Gonzales <erichelouise@gmail.com>
2022-07-28 13:57:14 -0400

    Done with second dlvb

--- src/components/Planeteer.js
+++ src/components/Planeteer.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Planeteer({ planeteer }) {
   const { id, name, fromUSA, born, bio, quote, pictureUrl, twitter} = planeteer
+  const [showBio, setShowBio] = useState(true)
+
+  const toggleBio = () => {
+    setShowBio(showBio => !showBio)
+    console.log(showBio)
+  }
 
   return (
     <li className="cards__item">
@@ -10,10 +16,12 @@ function Planeteer({ planeteer }) {
           src={pictureUrl}
           alt={name}
           className="card__image"
+          onClick={toggleBio}
         />
         <div className="card__content">
           <div className="card__title">{name}</div>
-          <p className="card__text">{bio}</p>
+          <p className="card__text">{showBio ? 
+            `Bio: ${bio}` : `Quote: ${quote}`}</p>
           <div className="card__detail">
             <p>{twitter}</p>
             <p>
